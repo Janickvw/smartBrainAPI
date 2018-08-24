d6ce8ba92dad97d95e1235c0272a960ee05c515c
Janickvw <janick.van.well@gmail.com>
2018-08-24 19:35:08 +1200

    Changed register response

--- controllers/register.js
+++ controllers/register.js
@@ -20,7 +20,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 					name: name,
 					joined: new Date()
 				})
-				.then(response => res.json(response))
+				.then(user => res.json(user[0]))
 		})
 		.then(trx.commit)
 		.catch(trx.rollback)

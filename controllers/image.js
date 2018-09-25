69f1064811486165030b69a60db71ad515f9bb41
Janickvw <janick.van.well@gmail.com>
2018-09-25 14:26:02 +1200

    prettified

--- controllers/image.js
+++ controllers/image.js
@@ -1,27 +1,28 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'b13f10753cf34e478413bf8ba74e736c'
+  apiKey: 'b13f10753cf34e478413bf8ba74e736c'
 });
 
 const handleApiCall = (req, res) => {
-	app.models
-		.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
-		.then(data => res.json(data))
-		.catch(err => res.status(400).json('Unable to work with API'))
-}
+  app.models
+    .predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('Unable to work with API'));
+};
 
 const handleImage = (req, res, db) => {
-	const { id } = req.body;
+  const { id } = req.body;
 
-	db('users').where('id', '=', id)
-		.increment('entries', 1)
-		.returning('entries')
-		.then(entries => res.json(entries[0]))
-		.catch(err => res.status(400).json('Unable to get entries'))
-}
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('Unable to get entries'));
+};
 
 module.exports = {
-	handleImage: handleImage,
-	handleApiCall: handleApiCall
-}
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
-const image = require('./controllers/image')
+const image = require('./controllers/image');
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: true
-	}
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
 });
 
 const app = express();
@@ -22,14 +22,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Working'))
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-
+app.get('/', (req, res) => res.send('Working'));
+app.post('/signin', (req, res) => {
+  signin.handleSignIn(req, res, db, bcrypt);
+});
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`App is running on port ${process.env.PORT}`);
-})
+  console.log(`App is running on port ${process.env.PORT}`);
+});
